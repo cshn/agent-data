62b47eb7d8a9f6d2a22ce53a18d5d57617db8f8a
Sun Hainan <hainan.sun@gmail.com>
2019-07-19 07:51:27 +0800

    Add bootstrap and dashboard layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 import { HttpClientModule }    from '@angular/common/http';
 import { AgGridModule } from 'ag-grid-angular';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    SideBarComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModule,
     AgGridModule
   ],
   providers: [],
